9b737826f533a3e8d825455cc9ffd9c95a172e26
Johan Egneblad <jegneblad@vizrt.com>
2014-08-27 09:49:20 +0200

    Setup scripts and travis config

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+name_ = 'restnote'
+version_ = '0.0.1'
+packages_ = [
+    'restnote',
+]
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: POSIX",
+    "Operating System :: POSIX :: Linux",
+    "Topic :: Text Processing :: Markup",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Other Scripting Engines",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Text Processing :: Markup :: XML",
+]
+
+setup(
+    name=name_,
+    version=version_,
+    author='Johan Egneblad',
+    author_email='johan.egneblad@DELETEMEgmail.com',
+    description='Rest scripting layer with notebook generation',
+    license="MIT",
+    url='https://github.com/eblade/'+name_,
+    download_url=('https://github.com/eblade/%s/archive/v%s.tar.gz'
+                  % (name_, version_)),
+    packages=packages_,
+    scripts=['bin/restnote'],
+    classifiers = classifiers
+)
